df9e7956f604f84aea2ac16fa03825dd2f765e21
Yahor Silich <yahor.silich2@gmail.com>
2026-09-07 11:34:43 +0200

    Add king check validation, pawn promotion, and castling logic

--- chess.cpp
+++ chess.cpp
@@ -8,6 +8,12 @@ using namespace std;
 
 const int BOARD_SIZE = 8;
 
+struct CastlingRights {
+    bool wk = false, bk = false;
+    bool wrA = false, wrH = false;
+    bool brA = false, brH = false;
+};
+
 void printBoard(const vector<vector<char>>& board) {
     cout << "\n  a b c d e f g h\n";
     for (int row = 0; row < BOARD_SIZE; ++row) {
@@ -42,32 +48,67 @@ bool isValidPieceMove(char piece, int r1, int c1, int r2, int c2, bool isCapture
     int abs_dr = abs(dr);
     int abs_dc = abs(dc);
 
-    if (p == 'r') { 
+    if (p == 'r') {
         return (dr == 0 || dc == 0) && isPathClear(board, r1, c1, r2, c2);
-    } else if (p == 'b') { 
+    } else if (p == 'b') {
         return (abs_dr == abs_dc) && isPathClear(board, r1, c1, r2, c2);
-    } else if (p == 'q') { 
+    } else if (p == 'q') {
         return (dr == 0 || dc == 0 || abs_dr == abs_dc) && isPathClear(board, r1, c1, r2, c2);
-    } else if (p == 'n') { 
+    } else if (p == 'n') {
         return (abs_dr == 2 && abs_dc == 1) || (abs_dr == 1 && abs_dc == 2);
-    } else if (p == 'k') { 
+    } else if (p == 'k') {
         return abs_dr <= 1 && abs_dc <= 1;
-    } else if (p == 'p') { 
-        int dir = isupper(piece) ? -1 : 1; 
+    } else if (p == 'p') {
+        int dir = isupper(piece) ? -1 : 1;
         int startRow = isupper(piece) ? 6 : 1;
         
         if (!isCapture && dc == 0) {
-            if (dr == dir) return true; 
-            if (r1 == startRow && dr == 2 * dir && board[r1 + dir][c1] == '.') return true; 
+            if (dr == dir) return true;
+            if (r1 == startRow && dr == 2 * dir && board[r1 + dir][c1] == '.') return true;
         } else if (isCapture && abs_dc == 1 && dr == dir) {
-            return true; 
+            return true;
         }
         return false;
     }
     return false;
 }
 
-bool makeMove(vector<vector<char>>& board, string move, bool& isWhiteTurn) {
+bool isSquareAttacked(int targetRow, int targetCol, bool byWhite, const vector<vector<char>>& board) {
+    for (int r = 0; r < BOARD_SIZE; ++r) {
+        for (int c = 0; c < BOARD_SIZE; ++c) {
+            char piece = board[r][c];
+            if (piece == '.') continue;
+            
+            bool isWhitePiece = isupper(piece);
+            if (isWhitePiece == byWhite) {
+                if (isValidPieceMove(piece, r, c, targetRow, targetCol, true, board)) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+bool isKingInCheck(bool whiteKing, const vector<vector<char>>& board) {
+    char targetKing = whiteKing ? 'K' : 'k';
+    int kr = -1, kc = -1;
+    
+    for (int r = 0; r < BOARD_SIZE; ++r) {
+        for (int c = 0; c < BOARD_SIZE; ++c) {
+            if (board[r][c] == targetKing) {
+                kr = r;
+                kc = c;
+                break;
+            }
+        }
+        if (kr != -1) break;
+    }
+    
+    return isSquareAttacked(kr, kc, !whiteKing, board);
+}
+
+bool makeMove(vector<vector<char>>& board, string move, bool& isWhiteTurn, CastlingRights& cr) {
     if (move.length() != 4) return false;
 
     int fromCol = move[0] - 'a';
@@ -93,13 +134,71 @@ bool makeMove(vector<vector<char>>& board, string move, bool& isWhiteTurn) {
         if (isWhiteTurn && isupper(targetPiece)) return false;
         if (!isWhiteTurn && islower(targetPiece)) return false;
     }
-    if (!isValidPieceMove(piece, fromRow, fromCol, toRow, toCol, isCapture, board)) {
+
+    bool isCastling = false;
+    if (tolower(piece) == 'k' && abs(toCol - fromCol) == 2) {
+        if (fromRow != toRow) return false;
+        if (isWhiteTurn ? cr.wk : cr.bk) return false;
+        
+        if (toCol == 6) {
+            if (isWhiteTurn ? cr.wrH : cr.brH) return false;
+            if (!isPathClear(board, fromRow, 4, fromRow, 7)) return false;
+            if (isSquareAttacked(fromRow, 4, !isWhiteTurn, board) || 
+                isSquareAttacked(fromRow, 5, !isWhiteTurn, board) || 
+                isSquareAttacked(fromRow, 6, !isWhiteTurn, board)) return false;
+        } else if (toCol == 2) {
+            if (isWhiteTurn ? cr.wrA : cr.brA) return false;
+            if (!isPathClear(board, fromRow, 4, fromRow, 0)) return false;
+            if (isSquareAttacked(fromRow, 4, !isWhiteTurn, board) || 
+                isSquareAttacked(fromRow, 3, !isWhiteTurn, board) || 
+                isSquareAttacked(fromRow, 2, !isWhiteTurn, board)) return false;
+        } else {
+            return false;
+        }
+        isCastling = true;
+    } else if (!isValidPieceMove(piece, fromRow, fromCol, toRow, toCol, isCapture, board)) {
         return false;
     }
 
     board[toRow][toCol] = piece;
     board[fromRow][fromCol] = '.';
-    
+
+    if (isCastling) {
+        if (toCol == 6) {
+            board[toRow][5] = board[toRow][7];
+            board[toRow][7] = '.';
+        } else {
+            board[toRow][3] = board[toRow][0];
+            board[toRow][0] = '.';
+        }
+    }
+
+    if (isKingInCheck(isWhiteTurn, board)) {
+        board[fromRow][fromCol] = piece;
+        board[toRow][toCol] = targetPiece;
+        if (isCastling) {
+            if (toCol == 6) {
+                board[toRow][7] = board[toRow][5];
+                board[toRow][5] = '.';
+            } else {
+                board[toRow][0] = board[toRow][3];
+                board[toRow][3] = '.';
+            }
+        }
+        return false;
+    }
+
+    if (piece == 'K') cr.wk = true;
+    if (piece == 'k') cr.bk = true;
+    if (piece == 'R' && fromCol == 0) cr.wrA = true;
+    if (piece == 'R' && fromCol == 7) cr.wrH = true;
+    if (piece == 'r' && fromCol == 0) cr.brA = true;
+    if (piece == 'r' && fromCol == 7) cr.brH = true;
+
+    if (tolower(piece) == 'p' && (toRow == 0 || toRow == 7)) {
+        board[toRow][toCol] = isWhiteTurn ? 'Q' : 'q';
+    }
+
     isWhiteTurn = !isWhiteTurn;
     return true;
 }
@@ -117,12 +216,16 @@ int main() {
     };
 
     bool isWhiteTurn = true;
+    CastlingRights cr;
     string move;
 
     cout << "Welcome to SilichChess!\n";
     printBoard(board);
 
     while (true) {
+        if (isKingInCheck(isWhiteTurn, board)) {
+            cout << "CHECK! ";
+        }
         cout << (isWhiteTurn ? "White's turn: " : "Black's turn: ");
         cin >> move;
 
@@ -131,7 +234,7 @@ int main() {
             break;
         }
 
-        if (makeMove(board, move, isWhiteTurn)) {
+        if (makeMove(board, move, isWhiteTurn, cr)) {
             printBoard(board);
         } else {
             cout << "Invalid move! Try again.\n";
